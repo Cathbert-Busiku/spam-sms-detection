87d4dd6a7ebb04d7780aa091007af94a3eb94782
Cathbert Busiku <59375364+Cathbert-Busiku@users.noreply.github.com>
2022-05-28 21:52:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ from nltk.corpus import stopwords
 
 app = Flask(__name__)
 
-# def text_process(mess):
-#     """
-#     Takes in a string of text, then performs the following:
-#     1. Remove all punctuation
-#     2. Remove all stopwords
-#     3. Returns a list of the cleaned text
-#     """
-#     # Check characters to see if they are in punctuation
-#     nopunc = [char for char in mess if char not in string.punctuation]
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in mess if char not in string.punctuation]
 
-#     # Join the characters again to form the string.
-#     nopunc = ''.join(nopunc)
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
     
-#     # Now just remove any stopwords
+    # Now just remove any stopwords
     
-#     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
 @app.route('/',methods=['GET'])
 def home():
